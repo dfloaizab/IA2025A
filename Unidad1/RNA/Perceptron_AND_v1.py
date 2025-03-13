8bb0273175ff84de3f467e5262cd4789726305de
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-03-13 14:15:46 -0500

    Update Perceptron_AND_v1.py

--- Unidad1/RNA/Perceptron_AND_v1.py
+++ Unidad1/RNA/Perceptron_AND_v1.py
@@ -7,8 +7,25 @@ X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 0, 0, 1])
 
 # Crear y entrenar el Perceptrón
-model = Perceptron(max_iter=1000, eta0=0.1, random_state=0)
-model.fit(X, y)
+model = Perceptron(max_iter=1000, eta0=0.1, random_state=0,verbose=0)
+
+#mostrar error obtenido en cada iteración:
+errors = []
+
+for __ in range(10):
+    model.partial_fit(X,y,classes= np.array([0,1]))
+    #obtener error de la iteración:
+    errors.append( (y!=model.predict(X)).sum() )
+
+print(f"\n Resultado final del perceptrón:")
+print(f"pesos finales para las entradas:{model.coef_}")
+print(f"bias final:{model.intercept_}")
+print(f"Número de iteraciones efectivas:{model.n_iter_}")
+
+#Parámetros de confiabilidad del perceptrón:
+accuracy = model.score(X,y) * 100
+
+#model.fit(X, y)
 
 # Predicciones
 print(model.predict(X))
